3d93670481d88f04b298ba2f60a59fb49f31db04
Melisa <melisa.pinto@kpmg.co.uk>
2020-08-05 11:07:23 +0100

    Added in answers to first exercise

---
+++ session_01/answers/answers.py
@@ -0,0 +1,39 @@
+# Write code that prints ‘Hello world’
+print("Hello world")
+
+#Print the numbers 1 to 5 on a single line.
+print("1 2 3 4 5")
+
+#Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
+print ("Hello\nWorld")
+
+#Write a script that prints a list of names, tabbed on separate lines, e.g.
+#
+#             Alice
+#             Bob
+#             Charlie
+print("\n\ttAlice\n\tBob\n\tCharlie")
+
+# Write code that prints the value of 2 + 2.
+print (2+2)
+
+# Write code that prints the value of 5.7 subtracted from 3.4
+print (5.7-3.4)
+
+# Write code that prints the value of 8 multiplied by 7
+print (8*7)
+
+# Write code that prints the value of 144 divided by 12
+print (144/12)
+
+# Write code that prints the value of the remainder of 67 divided by 12
+x = 67 % 12
+print (x)
+
+# Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets.
+print (2+2)
+y = (4 - 2) * 6 / 3 * 5
+print(y)
+
+
+
